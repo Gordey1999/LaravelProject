06ae9a6b22c300505b865e28271233e2c81fd1c6
Gordey <gordey_enot@mail.ru>
2020-11-15 10:58:05 +0300

    =ide helper

--- config/app.php
+++ config/app.php
@@ -145,7 +145,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
-		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
 
 	],
 
