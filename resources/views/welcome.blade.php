f7a53a70f70dde8b490706caaf8b5f0dd4aa5b0a
Zhizhin <gordey_enot@mail.ru>
2020-11-16 16:19:25 +0800

    Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,26 @@
 <html>
 	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
+		<title>Welcome</title>
 		<style>
 			body {
 				margin: 0;
 				padding: 0;
 				width: 100%;
 				height: 100%;
-				color: #B0BEC5;
+				color: #ffffffb3;
 				display: table;
 				font-weight: 100;
-				font-family: 'Lato';
+                font-family: system-ui;
+                background-image: url("/images/the-great-gatsby-movie.jpg");
+                background-position: center;
+                background-size: cover;
 			}
 
 			.container {
 				text-align: center;
 				display: table-cell;
 				vertical-align: middle;
+                padding-top: 10px;
 			}
 
 			.content {
@@ -30,18 +31,40 @@
 			.title {
 				font-size: 96px;
 				margin-bottom: 40px;
+                letter-spacing: 0.7em;
 			}
 
 			.quote {
 				font-size: 24px;
 			}
+
+            a {
+                letter-spacing: 0.7em;
+                color: #ffffffb3;
+                text-decoration: none;
+                border-bottom: 1px dashed #ffffffb3;
+                transition-duration: 500ms;
+                line-height: 32px;
+            }
+
+            a span {
+                letter-spacing: 0;
+            }
+
+            a:hover {
+                color: white;
+                border-color: white;
+                transition-duration: 500ms;
+                font-size: 28px;
+            }
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">велкоме</div>
+				{{--<div class="quote">{{ Inspiring::quote() }}</div>--}}
+                <div class="quote"><a href="/home">хоум<span>е</span></a></div>
 			</div>
 		</div>
 	</body>
